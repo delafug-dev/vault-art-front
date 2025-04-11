c701a3c127923e2b72fcd89cfa1fb04d310aa651
delafug-dev <adelafuentegarceso@gmail.com>
2025-04-11 13:39:12 +0200

    Adaptando el Page a Nextjs 15 con params

--- src/app/artists/[slug]/page.tsx
+++ src/app/artists/[slug]/page.tsx
@@ -1,15 +1,13 @@
+'use server'
 import Artist from "@/modules/artists/components/ArtistPage/Artist"
 
-interface ArtistPageProps {
-    params: {
-        slug: string
-    }
-}
+// Actualiza la definición de tipos para reflejar que params es ahora una Promise
+export default async function Page({ params }: {
+    params: Promise<{ slug: string }>
+}) {
+    // Espera a que se resuelva la promesa params
+    const resolvedParams = await params;
 
-const ArtistPage = ({ params } : ArtistPageProps) => {
-    return (
-        <Artist params={params}/>
-    )
+    // Pasa los parámetros resueltos al componente Artist
+    return <Artist params={resolvedParams} />
 }
-
-export default ArtistPage
