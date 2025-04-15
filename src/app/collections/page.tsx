97693363c1bca03efc2d2328210b4a9d0639e40f
delafug-dev <adelafuentegarceso@gmail.com>
2025-04-15 14:38:58 +0200

    Collections page in progress

---
+++ src/app/collections/page.tsx
@@ -0,0 +1,11 @@
+import Collections from '@/modules/artwork/components/CollectionsPage/Collections'
+
+const CollectionsPage = () => {
+
+    return (
+        <Collections />
+    )
+    
+}
+
+export default CollectionsPage
